884a78ec6413e20bdf07ae43e38041425d7e8928
Andax <anhuang@student.42.fr>
2019-07-30 14:12:22 +0200

    Get Count Inversions OK

--- board_info.py
+++ board_info.py
@@ -71,4 +71,59 @@ class BoardInfo():
 				size -= 1
 			if (dir is 5):
 				dir = 1
+		print(ret)
 		return ret
+
+	def countInversion(self, snail = 1):
+		tab = [0] * self.size**2
+		tab = self.node.getAll()
+		p = [0] * self.size**2
+		p = self.node.getAll()
+		count = 0
+
+		if (snail is 0):
+			for i in range(self.size**2):
+				a = 0
+				while (tab[i] is not 0 and a + i < self.size**2):
+					if (tab[i + a] is not 0 and tab[i + a] < tab[i]):
+						count += 1
+					a += 1
+			print("Count Inversion :", end=" ")
+			return count
+			
+		dir = 1
+		size = self.size - 1
+		index = size
+		s_index = index
+		while (size):
+			if dir is 1 :
+				for m_size in range(size):
+					tab[index + m_size + 1] = p[s_index + (m_size + 1) * self.size]
+				s_index += size * self.size
+			elif dir is 2 :
+				for m_size in range(size):
+					tab[index + m_size + 1] = p[s_index - (m_size + 1)]
+				s_index -= size
+			elif dir is 3 :
+				for m_size in range(size):
+					tab[index + m_size + 1] = p[s_index - (m_size + 1) * self.size]
+				s_index -= size * self.size
+			elif dir is 4 :
+				for m_size in range(size):
+					tab[index + m_size + 1] = p[s_index + m_size + 1]
+				s_index += size
+			index += size
+			dir += 1
+			if (dir % 2):
+				size -= 1
+			if (dir is 5):
+				dir = 1
+		print("GRID SNAIL :", tab)
+		for i in range(self.size**2):
+			a = 0
+			while (tab[i] is not 0 and a + i < self.size**2):
+				if (tab[i + a] is not 0 and tab[i + a] < tab[i]):
+					count += 1
+				a += 1
+		print("Count Inversion :", end=" ")
+		return count
